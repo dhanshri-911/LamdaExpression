9c26db4ad462f63307a071fba099a21de39b6554
Dhanshri911 <dszingade911@gmail.com>
2022-04-13 00:22:31 +0530

    UC7:Added Passwor Rule Three Atleast One Numeric Number

--- src/com/bridgelabz/PasswordRuleThree.java
+++ src/com/bridgelabz/PasswordRuleThree.java
@@ -4,23 +4,24 @@ import java.util.regex.Pattern;
 
 
 @FunctionalInterface
-interface IPasswordTwo {
+interface IPasswordThree {
     public String validate(String regex, String field);
 }
 
-public class PasswordRuleTwo {
+public class PasswordRuleThree {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please log in using 8 character password having one upper case.");
+        System.out.println("Please log in using 8 character password having one upper case and one numeric number.");
         System.out.print("Enter the password : ");
         String field = sc.next();
-        IPasswordTwo isValiLastName = (pattern, passWord) -> {
+        IPasswordThree isValiLastName = (pattern, passWord) -> {
 
             return "The password you have entered is " + Pattern.compile(pattern).matcher(passWord).matches() + ".";
         };
 
-        System.out.println(isValiLastName.validate("^(?=.*[A-Z])[A-Za-z]{8}$", field));
+        System.out.println(isValiLastName.validate("^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{8,}$", field));
 
     }
 
 }
+
