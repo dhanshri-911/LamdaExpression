f88ba01470e358ac5ae09f1f05cfc81abdbf73a1
Dhanshri911 <dszingade911@gmail.com>
2022-04-13 00:26:30 +0530

    UC8:Added Passwor Rule Three Atleast One Special Char

--- src/com/bridgelabz/PasswordRuleFour.java
+++ src/com/bridgelabz/PasswordRuleFour.java
@@ -4,22 +4,25 @@ import java.util.regex.Pattern;
 
 
 @FunctionalInterface
-interface IPasswordThree {
+interface IPasswordFour {
     public String validate(String regex, String field);
 }
 
-public class PasswordRuleThree {
+public class PasswordRuleFour {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please log in using 8 character password having one upper case and one numeric number.");
+        System.out.println(
+                "Please log in using 8 character password having one upper case and one numeric number and has exactly 1 Special Character.");
         System.out.print("Enter the password : ");
         String field = sc.next();
-        IPasswordThree isValiLastName = (pattern, passWord) -> {
+        IPasswordFour isValiLastName = (pattern, passWord) -> {
 
             return "The password you have entered is " + Pattern.compile(pattern).matcher(passWord).matches() + ".";
         };
 
-        System.out.println(isValiLastName.validate("^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{8,}$", field));
+        System.out.println(
+                isValiLastName.validate("^(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*#?&])[A-Za-z0-9@$!%*#?&]{8,}$", field));
 
     }
 
