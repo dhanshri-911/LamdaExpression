15932fedce4ae240cc329d906fd0b2e786960e76
Dhanshri911 <dszingade911@gmail.com>
2022-04-13 00:13:48 +0530

    UC5:Added Passwor Rule 1 min 8 char

---
+++ src/com/bridgelabz/PasswordRuleOne.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+
+@FunctionalInterface
+interface IPassword {
+    public String validate(String regex, String field);
+}
+
+public class PasswordRuleOne {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please log in using 8 character password.");
+        System.out.print("Enter the Password : ");
+        String field = sc.next();
+        IPassword isValidPassword = (pattern, passWord) -> {
+
+            return "The password you have entered is " + Pattern.compile(pattern).matcher(passWord).matches()+ ".";
+        };
+        System.out.println(isValidPassword.validate("^[A-Za-z]{8}$", field));
+        System.out.println("");
+
+    }
+
+}
